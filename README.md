4bf4a9a3fa2d88a2801a994a8578516d857a5bfc
ABC2312 <1851707082@qq.com>
2024-08-16 16:37:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lease
+# lease
+# studytest
---
+++ common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.atguigu.lease.common.utils;
+
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtUtil {
+    private static SecretKey secretKey = Keys.hmacShaKeyFor("tdsAdS3W2qpPHCD9HbnJmlJcHoKzvvLg".getBytes());
+
+    public static String createToken(Long userId, String username) {
+        String jwt = Jwts.builder()
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .setSubject("LOGIN_USER")
+                .claim("userId", userId)
+                .claim("username", username)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+        return jwt;
+    }
+
+    public static Claims parseToken(String token) {
+
+        if (token == null || token.isEmpty()) {
+            throw new LeaseException(ResultCodeEnum.TOKEN_INVALID);
+        }
+        try {
+            JwtParser jwtParser = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build();
+            Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
+            return claimsJws.getBody();
+        } catch (ExpiredJwtException e) {
+            throw new LeaseException(ResultCodeEnum.TOKEN_EXPIRED);
+        } catch (JwtException e) {
+            throw new LeaseException(ResultCodeEnum.TOKEN_INVALID);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createToken(8L, "19513571089"));
+    }
+}
